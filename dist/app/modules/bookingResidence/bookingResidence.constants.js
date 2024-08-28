bc39c163c9c423a6d77326079f320e2bfe1a8238
mdnazmulhasanniloy <mdnazmulhasanniloy323@gmail.com>
2024-08-28 18:25:33 +0600

    some changes

--- dist/app/modules/bookingResidence/bookingResidence.constants.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.bookingStatus = void 0;
-exports.bookingStatus = ['pending', 'confirmed', 'cancelled'];
--- src/app/modules/residence/residence.utils.ts
+++ src/app/modules/residence/residence.utils.ts
@@ -4,6 +4,7 @@ interface IResult {
   averageRating: number;
   totalReview: number;
 }
+
 export const calculateAverageRatingForResidence = async (
   residenceId: string,
 ): Promise<IResult | null> => {
@@ -18,20 +19,53 @@ export const calculateAverageRatingForResidence = async (
       $group: {
         _id: '$residence',
         averageRating: { $avg: '$rating' },
-        totalReview: { $sum: '$rating' },
+        totalReview: { $sum: 1 }, // Count the number of reviews
       },
     }, // Group by residence and calculate the average rating
   ]);
 
-  if (result.length > 0) { 
+  if (result.length > 0) {
     return {
-      averageRating: Number(result[0].averageRating.toFixed(1)||0),
-      totalReview: Number(result[0].totalReview.toFixed(1)||0),
+      averageRating: Number(result[0].averageRating.toFixed(1) || 0),
+      totalReview: result[0].totalReview, // Return the total number of reviews
     };
   }
+
   const defaultNumber: number = 0;
   return {
     averageRating: Number(defaultNumber.toFixed(1)),
-    totalReview: Number(defaultNumber.toFixed(1)),
-  }; // No ratings found
+    totalReview: defaultNumber, // No ratings found
+  };
 };
+
+// export const calculateAverageRatingForResidence = async (
+//   residenceId: string,
+// ): Promise<IResult | null> => {
+//   const result = await Review.aggregate([
+//     {
+//       $match: {
+//         residence: new Types.ObjectId(residenceId),
+//         isDeleted: { $ne: true },
+//       },
+//     }, // Match the residence and filter out deleted reviews
+//     {
+//       $group: {
+//         _id: '$residence',
+//         averageRating: { $avg: '$rating' },
+//         totalReview: { $sum: '$rating' },
+//       },
+//     }, // Group by residence and calculate the average rating
+//   ]);
+
+//   if (result.length > 0) {
+//     return {
+//       averageRating: Number(result[0].averageRating.toFixed(1)||0),
+//       totalReview: Number(result[0].totalReview.toFixed(1)||0),
+//     };
+//   }
+//   const defaultNumber: number = 0;
+//   return {
+//     averageRating: Number(defaultNumber.toFixed(1)),
+//     totalReview: Number(defaultNumber.toFixed(1)),
+//   }; // No ratings found
+// };
